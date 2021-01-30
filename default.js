401ba0e06dd3809c74d969659e337205a40cffab
Tanviras <tanvirasme@gmail.com>
2021-01-30 12:10:38 +0600

    done

---
+++ default.js
@@ -0,0 +1,11 @@
+function add(num1,num2=20){//default value of num2 is set,it will work unless a value of num2 is given.
+    return num1+num2; 
+}
+
+console.log(add(15));
+
+function add(num1,num2=20){//default value of num2 is set,it will work unless a value of num2 is given.
+    return num1+num2; 
+}
+
+console.log(add(15,17));
